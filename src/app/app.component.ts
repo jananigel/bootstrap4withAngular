4335c55b1ee6be01521c354b0d48afed0b463a02
jananigel <jananigel@hotmail.com>
2018-01-08 01:47:54 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
+import 'bootstrap';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+
+  ngAfterViewInit(){
+    $('[data-toggle="tooltip"]').tooltip();
+  }
 }
